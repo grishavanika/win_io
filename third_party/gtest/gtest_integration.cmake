2d4a0832e20a72ac6addb13d0ec3cd78102dce8d
grisha vanika <grish.vanika@gmail.com>
2018-05-22 19:32:18 +0300

    Speed-up compilation/generation time for regular MSVC by using pre-installed (with vcpkg) gtest lib

--- third_party/gtest/gtest_integration.cmake
+++ third_party/gtest/gtest_integration.cmake
@@ -1,11 +1,5 @@
 # GoogleTest integration
 
-# GoogleTest uses static C++ runtime by default,
-# use C++ as dll instead
-set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-
-add_subdirectory(gtest)
-
 # Using macro since most of directives should be repeated 3 times
 # (for gtest, gmock_main and gmock). Using target_compile*() command
 # with PUBLIC argument does not help (#TODO: investigate why)
@@ -40,6 +34,38 @@ macro(setup_gtest_lib lib_name)
 
 endmacro()
 
-setup_gtest_lib(gtest)
-setup_gtest_lib(gmock_main)
-setup_gtest_lib(gmock)
+macro(setup_gtest_manually)
+	# GoogleTest uses static C++ runtime by default,
+	# use C++ as dll instead
+	set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
+	add_subdirectory(gtest)
+
+	setup_gtest_lib(gtest)
+	setup_gtest_lib(gmock_main)
+	setup_gtest_lib(gmock)
+
+	add_library(gtest_all INTERFACE)
+	target_link_libraries(gtest_all INTERFACE gtest gmock_main)
+endmacro()
+
+if (MSVC)
+	# Assume that Clang and GCC for Windows both have no pre-built libraries
+	# (Like with vcpkg)
+	find_package(GTest)
+endif()
+
+if (${GTest_FOUND})
+	add_library(gtest_all INTERFACE)
+	target_link_libraries(gtest_all INTERFACE GTest::GTest GTest::Main)
+	if (MSVC)
+		target_compile_options(gtest_all INTERFACE
+			# class needs to have dll-interface to be used by clients
+			/wd4251
+			# non–DLL-interface used as base for DLL-interface
+			/wd4275)
+	endif()
+else()
+	setup_gtest_manually()
+endif()
+
--- tools/generate_project.py
+++ tools/generate_project.py
@@ -5,6 +5,18 @@ import os
 from enum import Enum
 import argparse
 
+def quote(str):
+	return '"' + str + '"'
+
+def quote_spaces(str):
+	if ' ' in str:
+		return quote(str)
+	else:
+		return str
+
+def to_cmd_line(args):
+	return ' '.join([quote_spaces(arg) for arg in args])
+
 class Compiler(Enum):
 	CL = 1
 	CLANG = 2
@@ -27,10 +39,11 @@ def enum_cmd_values(enum):
 	return [enum_str(c) for c in enum]
 
 class Configuration:
-	def __init__(self, compiler = Compiler.CLANG, platform = Platform.X64):
+	def __init__(self, compiler = Compiler.CLANG, platform = Platform.X64, vcpkg_path = None):
 		self.compiler = compiler
 		self.platform = platform
-	
+		self.vcpkg_path = vcpkg_path
+
 	def build_folder_name(self):
 		return 'build_{}_{}'.format(enum_str(self.compiler), enum_str(self.platform))
 
@@ -43,11 +56,14 @@ def cmake_win_generator_args(config):
 	vs_name = 'Visual Studio 15 2017'
 	if config.platform == Platform.X64:
 		vs_name += ' Win64'
-	
+
 	args = ['-G', vs_name]
 
 	if config.compiler == Compiler.CLANG:
 		args += ['-T', 'LLVM-vs2014']
+
+	if config.vcpkg_path:
+		args += ['-DCMAKE_TOOLCHAIN_FILE={}'.format(config.vcpkg_path)]
 
 	return args
 
@@ -68,7 +84,10 @@ def find_parent_cmake_folder():
 	return path
 
 def invoke_cmake(args):
-	return subprocess.call(['cmake'] + args)
+	full_cmd = ['cmake'] + args
+	print('Invoking CMake:')
+	print(to_cmd_line(full_cmd))
+	return subprocess.call(full_cmd)
 
 def cmake_generate(config, location, args):
 	return invoke_cmake(cmake_generator_args(config)
@@ -86,12 +105,14 @@ def cmd_line_parser():
 		, choices = enum_cmd_values(Compiler), default = enum_str(Compiler.CLANG))
 	parser.add_argument('--platform'
 		, choices = enum_cmd_values(Platform), default = enum_str(Platform.X64))
+	parser.add_argument('--vcpkg')
 	return parser
 
 def configuration_from_args(args):
 	return Configuration(
 		enum_from_string(Compiler, args.compiler),
-		enum_from_string(Platform, args.platform))
+		enum_from_string(Platform, args.platform),
+		vcpkg_path = args.vcpkg)
 
 def do_main():
 	args = cmd_line_parser().parse_args()
