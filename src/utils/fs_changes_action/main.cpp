bab36ece13386eeda885a6c2eff81fd8fd6a4dbf
grisha vanika <grish.vanika@gmail.com>
2019-07-26 22:15:33 +0300

    Initial and ugly-ugly version of file system changes monitoring with RxCpp.

--- src/utils/fs_changes_action/main.cpp
+++ src/utils/fs_changes_action/main.cpp
@@ -1,15 +1,88 @@
 #include <win_io/detail/read_directory_changes.h>
+#include <win_io/detail/io_completion_port.h>
+#include <win_io/detail/read_directory_changes.h>
 
+#include <string>
 #include <rxcpp/rx.hpp>
 
 #include <Windows.h>
 
-int wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance
-	, LPWSTR lpCmdLine, int nShowCmd)
+namespace io = wi::detail;
+
+namespace rx = rxcpp;
+namespace rxu = rxcpp::util;
+namespace rxs = rxcpp::subjects;
+namespace rxo = rxcpp::operators;
+
+int WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int)
 {
-    (void)hInstance;
-    (void)hPrevInstance;
-    (void)lpCmdLine;
-    (void)nShowCmd;
+	{ // Temporary make std::cout available
+		::AllocConsole();
+		FILE* unused;
+		freopen_s(&unused, "CONOUT$", "w", stdout);
+	}
+
+	io::IoCompletionPort io_service(1/*threads count*/);
+	rxs::subject<io::PortData> io_port_data;
+	rx::observable<io::PortData> io_changes = io_port_data.get_observable();
+	auto io_port = io_port_data.get_subscriber();
+	
+	const io::WinDWORD k_filters = 0
+		| FILE_NOTIFY_CHANGE_FILE_NAME
+		| FILE_NOTIFY_CHANGE_DIR_NAME
+		| FILE_NOTIFY_CHANGE_ATTRIBUTES
+		| FILE_NOTIFY_CHANGE_SIZE
+		| FILE_NOTIFY_CHANGE_LAST_WRITE
+		| FILE_NOTIFY_CHANGE_LAST_ACCESS
+		| FILE_NOTIFY_CHANGE_CREATION
+		| FILE_NOTIFY_CHANGE_SECURITY;
+
+	DWORD buffer[16 * 1024];
+	io::DirectoryChanges dir_changes(L"C:\\", buffer, sizeof(buffer)
+		, true/*watch_sub_tree*/, k_filters, io_service);
+
+	auto pipeline = io_changes
+		| rxo::transform([&](io::PortData pd)
+		{
+			return io::DirectoryChangesRange(buffer, pd);
+		})
+		| rxo::tap([&](io::DirectoryChangesRange changes)
+		{
+			for (const auto change : changes)
+			{
+				std::wcout.write(change.name.data(), change.name.size());
+				std::wcout << L"\n";
+			}
+			std::wcout.flush();
+
+			// #XXX: should be as separate action
+			dir_changes.start_watch();
+		})
+		| rxo::as_dynamic();
+
+	// #XXX: how to provide subscriber ?
+	// Convert cold to hot ?
+	pipeline.subscribe([](io::DirectoryChangesRange) {});
+
+
+	// Start watching
+	dir_changes.start_watch();
+
+	while (true)
+	{
+		std::error_code ec;
+		auto data = io_service.get(ec);
+		if (data)
+		{
+			io_port.on_next(std::move(*data));
+		}
+		else
+		{
+			// #XXX: errors should be pushed to pipeline
+			assert(ec);
+			std::cout << "ERROR: " << ec.message() << std::endl;
+		}
+	}
+
 	return 0;
 }
--- third_party/rxcpp/rxcpp_integration.cmake
+++ third_party/rxcpp/rxcpp_integration.cmake
@@ -8,6 +8,11 @@ if (EXISTS "${rxcpp_INCLUDE_DIR}")
 	message("Found RxCpp at ${rxcpp_INCLUDE_DIR}")
 	add_library(rxcpp INTERFACE)
 	target_include_directories(rxcpp INTERFACE ${rxcpp_INCLUDE_DIR})
+
+	target_compile_options(rxcpp INTERFACE 
+		# unreachable code
+		/wd4702)
+
 else ()
 	message(FATAL_ERROR "No rxcpp found")
 endif ()
