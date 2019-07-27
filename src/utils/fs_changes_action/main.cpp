484a61ca5a8fd346e0c99ee230d2f73cf181a8e4
grisha vanika <grish.vanika@gmail.com>
2019-07-27 13:51:26 +0300

    Split IO notifications into fine-grained streams.
    This allows to re-start directory watching as part of stream(s) processing.

--- src/utils/fs_changes_action/main.cpp
+++ src/utils/fs_changes_action/main.cpp
@@ -16,10 +16,10 @@ namespace rxo = rxcpp::operators;
 
 int WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int)
 {
-	{ // Temporary make std::cout available
-		::AllocConsole();
+	{ // Temporary make std::cout available, ignore any errors
+		(void)::AllocConsole();
 		FILE* unused;
-		freopen_s(&unused, "CONOUT$", "w", stdout);
+		(void)freopen_s(&unused, "CONOUT$", "w", stdout);
 	}
 
 	io::IoCompletionPort io_service(1/*threads count*/);
@@ -38,31 +38,66 @@ int WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int)
 		| FILE_NOTIFY_CHANGE_SECURITY;
 
 	DWORD buffer[16 * 1024];
-	io::DirectoryChanges dir_changes(L"C:\\", buffer, sizeof(buffer)
+	io::DirectoryChanges dir_watcher(L"C:\\", buffer, sizeof(buffer)
 		, true/*watch_sub_tree*/, k_filters, io_service);
 
-	auto pipeline = io_changes
-		| rxo::transform([&](io::PortData pd)
-		{
-			return io::DirectoryChangesRange(buffer, pd);
-		})
-		| rxo::flat_map([](io::DirectoryChangesRange range)
-		{
-			return rx::observable<>::iterate(range);
-		})
-		| rxo::as_dynamic();
+	using namespace rxo;
 
-	pipeline.subscribe(
-		[](io::DirectoryChange change)
-	{
-		std::wcout.write(change.name.data(), change.name.size());
-		std::wcout << std::endl;
-	});
+	auto dir_changes = io_changes
+		.filter([&](io::PortData pd)
+		{
+			return dir_watcher.is_valid_directory_change(pd);
+		})
+		.map([&](io::PortData pd)
+		{
+			return rx::observable<>::just(
+				io::DirectoryChangesRange(buffer, pd));
+		});
+	
+	auto dir_notifications = dir_changes
+		.flat_map([](rx::observable<io::DirectoryChangesRange> changes)
+		{
+			return changes.flat_map([](io::DirectoryChangesRange range)
+			{
+				return rx::observable<>::iterate(range);
+			});
+		});
+
+	auto draw_ui = dir_notifications
+		.tap([](io::DirectoryChange change)
+		{
+			std::wcout.write(change.name.data(), change.name.size());
+			std::wcout << L"\n";
+		});
+
+	auto flush_ui = dir_changes
+		// #XXX: how to ignore parameter ?
+		.tap([&](rx::observable<io::DirectoryChangesRange>)
+		{
+			std::wcout.flush();
+		});
+
+	auto repeat_watch = dir_changes
+		// #XXX: how to ignore parameter ?
+		.tap([&](rx::observable<io::DirectoryChangesRange>)
+		{
+			dir_watcher.start_watch();
+		});
+	
+	/////////////////////////////////////////////
+	// #XXX: how to just subscribe with no-op ?
+	// Make them hot ?
+	draw_ui.subscribe([](io::DirectoryChange) {});
+	flush_ui.subscribe([](rx::observable<io::DirectoryChangesRange>)
+		{ std::wcout << L"on changes" << std::endl; });
+	repeat_watch.subscribe([](rx::observable<io::DirectoryChangesRange>)
+		{ std::wcout << L"on repeat" << std::endl; });
+
+	/////////////////////////////////////////////
+	dir_watcher.start_watch();
 
 	while (true)
 	{
-		dir_changes.start_watch();
-
 		std::error_code ec;
 		auto data = io_service.get(ec);
 		if (data)
