a81734981a8b879854c8fd9a07d246a13df3c911
grishavanika <grish.vanika@gmail.com>
2021-10-18 19:51:02 +0300

    Implement async_accept().

--- src/examples/async_TCP_IOCP_senders_receivers/SenderReceiver_IOCP_Sockets.cpp
+++ src/examples/async_TCP_IOCP_senders_receivers/SenderReceiver_IOCP_Sockets.cpp
@@ -10,6 +10,7 @@
 #include <MSWSock.h>
 
 #pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "Mswsock.lib")
 
 #include <win_io/detail/io_completion_port.h>
 
@@ -39,6 +40,8 @@ static constexpr wi::WinULONG_PTR kClientKeyIOCP = 1;
 
 struct IOCP_Overlapped : OVERLAPPED
 {
+    LPOVERLAPPED ptr() { return this; }
+
     using Handle = void (*)(void* user_data, const wi::PortEntry& /*entry*/, std::error_code /*ec*/);
     Handle _callback = nullptr;
     void* _user_data = nullptr;
@@ -171,7 +174,7 @@ struct Operation_Connect : Operation_Log
 
         assert(entry.bytes_transferred == 0);
         assert(entry.completion_key == kClientKeyIOCP);
-        assert(entry.overlapped == &self._ov);
+        assert(entry.overlapped == self._ov.ptr());
 
         if (ec)
         {
@@ -226,13 +229,13 @@ struct Operation_Connect : Operation_Log
             , nullptr
             , 0
             , nullptr
-            , &self._ov);
+            , self._ov.ptr());
         if (finished)
         {
             wi::PortEntry entry;
             entry.bytes_transferred = 0;
             entry.completion_key = kClientKeyIOCP;
-            entry.overlapped = static_cast<LPOVERLAPPED>(&self._ov);
+            entry.overlapped = self._ov.ptr();
             self.on_connected(&self, entry, std::error_code());
             return;
         }
@@ -242,7 +245,7 @@ struct Operation_Connect : Operation_Log
             wi::PortEntry entry;
             entry.bytes_transferred = 0;
             entry.completion_key = kClientKeyIOCP;
-            entry.overlapped = static_cast<LPOVERLAPPED>(&self._ov);
+            entry.overlapped = self._ov.ptr();
             self.on_connected(&self, entry
                 , std::error_code(wsa_error, std::system_category()));
             return;
@@ -297,7 +300,7 @@ struct Operation_WriteSome : Operation_Log
 
         assert(entry.bytes_transferred > 0);
         assert(entry.completion_key == kClientKeyIOCP);
-        assert(entry.overlapped == &self._ov);
+        assert(entry.overlapped == self._ov.ptr());
 
         if (ec)
         {
@@ -314,6 +317,7 @@ struct Operation_WriteSome : Operation_Log
         self.log("start");
 
         assert(self._data.size() > 0);
+        assert(self._data.size() <= (std::numeric_limits<ULONG>::max)());
 
         WSABUF send_buffer{};
         send_buffer.buf = self._data.data();
@@ -322,14 +326,15 @@ struct Operation_WriteSome : Operation_Log
             , &send_buffer, 1
             , nullptr/*bytes_send*/
             , 0/*flags*/
-            , &self._ov, nullptr);
+            , self._ov.ptr()
+            , nullptr);
         if (error == 0)
         {
             // Completed synchronously.
             wi::PortEntry entry;
             entry.bytes_transferred = wi::WinDWORD(self._data.size());
             entry.completion_key = kClientKeyIOCP;
-            entry.overlapped = static_cast<LPOVERLAPPED>(&self._ov);
+            entry.overlapped = self._ov.ptr();
             self.on_sent(&self, entry, std::error_code());
             return;
         }
@@ -339,7 +344,7 @@ struct Operation_WriteSome : Operation_Log
             wi::PortEntry entry;
             entry.bytes_transferred = 0;
             entry.completion_key = kClientKeyIOCP;
-            entry.overlapped = static_cast<LPOVERLAPPED>(&self._ov);
+            entry.overlapped = self._ov.ptr();
             self.on_sent(&self, entry
                 , std::error_code(wsa_error, std::system_category()));
             return;
@@ -392,7 +397,7 @@ struct Operation_ReadSome : Operation_Log
         self.log("on_received");
 
         assert(entry.completion_key == kClientKeyIOCP);
-        assert(entry.overlapped == &self._ov);
+        assert(entry.overlapped == self._ov.ptr());
 
         if (ec)
         {
@@ -420,7 +425,7 @@ struct Operation_ReadSome : Operation_Log
             , &receive_buffer, 1
             , &received
             , &self._flags
-            , &self._ov
+            , self._ov.ptr()
             , nullptr);
         if (error == 0)
         {
@@ -428,7 +433,7 @@ struct Operation_ReadSome : Operation_Log
             wi::PortEntry entry;
             entry.bytes_transferred = received;
             entry.completion_key = kClientKeyIOCP;
-            entry.overlapped = static_cast<LPOVERLAPPED>(&self._ov);
+            entry.overlapped = self._ov.ptr();
             self.on_received(&self, entry, std::error_code());
             return;
         }
@@ -438,13 +443,14 @@ struct Operation_ReadSome : Operation_Log
             wi::PortEntry entry;
             entry.bytes_transferred = 0;
             entry.completion_key = kClientKeyIOCP;
-            entry.overlapped = static_cast<LPOVERLAPPED>(&self._ov);
+            entry.overlapped = self._ov.ptr();
             self.on_received(&self, entry
                 , std::error_code(wsa_error, std::system_category()));
             return;
         }
     }
 };
+
 struct Sender_ReadSome : Sender_LogSimple<std::size_t, std::error_code>
 {
     Sender_ReadSome(SOCKET socket, std::span<char> buffer) noexcept
@@ -466,6 +472,134 @@ struct Sender_ReadSome : Sender_LogSimple<std::size_t, std::error_code>
     }
 };
 
+template<typename Receiver
+    , typename _Async_TCPSocket /*= Async_TCPSocket*/>
+struct Operation_Accept : Operation_Log
+{
+    explicit Operation_Accept(Receiver&& receiver, SOCKET listen_socket, wi::IoCompletionPort& iocp)
+        : Operation_Log("receive_some")
+        , _receiver(std::move(receiver))
+        , _ov{{}, &Operation_Accept::on_accepted, this}
+        , _listen_socket(listen_socket)
+        , _iocp(&iocp)
+        , _buffer{}
+        , _client{} { }
+
+    Receiver _receiver;
+    IOCP_Overlapped _ov;
+    SOCKET _listen_socket = INVALID_SOCKET;
+    wi::IoCompletionPort* _iocp = nullptr;
+
+    // As per AcceptEx() and GetAcceptExSockaddrs():
+    // https://docs.microsoft.com/en-us/windows/win32/api/mswsock/nf-mswsock-acceptex
+    // https://docs.microsoft.com/en-us/windows/win32/api/mswsock/nf-mswsock-getacceptexsockaddrs.
+    static constexpr std::size_t kAddressLength = (sizeof(struct sockaddr) + 16);
+    char _buffer[2 * kAddressLength]{};
+    _Async_TCPSocket _client;
+
+    static void on_accepted(void* user_data, const wi::PortEntry& entry, std::error_code ec)
+    {
+        assert(user_data);
+        Operation_Accept& self = *static_cast<Operation_Accept*>(user_data);
+        self.log("on_accepted");
+
+        assert(entry.bytes_transferred == 0);
+        assert(entry.completion_key == kClientKeyIOCP);
+        assert(entry.overlapped == self._ov.ptr());
+
+        if (ec)
+        {
+            unifex::set_error(std::move(self._receiver), ec);
+            return;
+        }
+
+#if (0)
+        ::GetAcceptExSockaddrs(self._buffer
+            , 0
+            , Operation_Accept::kAddressLength
+            , Operation_Accept::kAddressLength
+            , )
+#endif
+        
+        const int error = ::setsockopt(self._client._socket, SOL_SOCKET, SO_UPDATE_ACCEPT_CONTEXT
+            , reinterpret_cast<char*>(&self._listen_socket)
+            , sizeof(self._listen_socket));
+        assert(error == 0);
+        // Close socket on error?
+
+        unifex::set_value(std::move(self._receiver), std::move(self._client));
+    }
+
+    friend void tag_invoke(unifex::tag_t<unifex::start>, Operation_Accept& self) noexcept
+    {
+        self.log("start");
+
+        std::error_code ec;
+        auto client = _Async_TCPSocket::make(*self._iocp, ec);
+        if (ec)
+        {
+            unifex::set_error(std::move(self._receiver), ec);
+            return;
+        }
+        assert(client);
+        self._client = std::move(*client);
+
+        using Self = Operation_Accept<Receiver, _Async_TCPSocket>;
+        const DWORD address_length = DWORD(Self::kAddressLength);
+        DWORD bytes_received = 0;
+        const BOOL ok = ::AcceptEx(self._listen_socket, self._client._socket
+            , self._buffer
+            , 0
+            , address_length
+            , address_length
+            , &bytes_received
+            , self._ov.ptr());
+        if (ok == TRUE)
+        {
+            // Completed synchronously.
+            wi::PortEntry entry;
+            entry.bytes_transferred = bytes_received;
+            entry.completion_key = kClientKeyIOCP;
+            entry.overlapped = self._ov.ptr();
+            self.on_accepted(&self, entry, std::error_code());
+            return;
+        }
+        const int wsa_error = ::WSAGetLastError();
+        if (wsa_error != ERROR_IO_PENDING)
+        {
+            wi::PortEntry entry;
+            entry.bytes_transferred = 0;
+            entry.completion_key = kClientKeyIOCP;
+            entry.overlapped = self._ov.ptr();
+            self.on_accepted(&self, entry
+                , std::error_code(wsa_error, std::system_category()));
+            return;
+        }
+    }
+};
+
+template<typename _Async_TCPSocket /*= Async_TCPSocket*/>
+struct Sender_Accept : Sender_LogSimple<_Async_TCPSocket, std::error_code>
+{
+    explicit Sender_Accept(SOCKET listen_socket, wi::IoCompletionPort& iocp) noexcept
+        : Sender_LogSimple<_Async_TCPSocket, std::error_code>("accept")
+        , _listen_socket(listen_socket)
+        , _iocp(&iocp) { }
+
+    SOCKET _listen_socket;
+    wi::IoCompletionPort* _iocp = nullptr;
+
+    template<typename This, typename Receiver>
+        requires std::is_same_v<Sender_Accept, std::remove_cvref_t<This>>
+    friend auto tag_invoke(unifex::tag_t<unifex::connect>
+        , This&& self, Receiver&& receiver) noexcept
+    {
+        self.log("connect");
+        using Reveiver_ = std::remove_cvref_t<Receiver>;
+        return Operation_Accept<Reveiver_, _Async_TCPSocket>(std::move(receiver), self._listen_socket, *self._iocp);
+    }
+};
+
 struct Async_TCPSocket
 {
     SOCKET _socket = INVALID_SOCKET;
@@ -473,8 +607,9 @@ struct Async_TCPSocket
 
     explicit Async_TCPSocket() = default;
 
-    static std::optional<Async_TCPSocket> make(wi::IoCompletionPort& iocp)
+    static std::optional<Async_TCPSocket> make(wi::IoCompletionPort& iocp, std::error_code& ec)
     {
+        ec = std::error_code();
         WSAPROTOCOL_INFOW* default_protocol = nullptr;
         GROUP no_group = 0;
         SOCKET handle = ::WSASocketW(AF_INET
@@ -486,6 +621,7 @@ struct Async_TCPSocket
         Async_TCPSocket socket(handle, iocp);
         if (handle == INVALID_SOCKET)
         {
+            ec = std::error_code(::WSAGetLastError(), std::system_category());
             return std::nullopt;
         }
 
@@ -494,6 +630,7 @@ struct Async_TCPSocket
             | FILE_SKIP_COMPLETION_PORT_ON_SUCCESS);
         if (!ok)
         {
+            ec = std::error_code(::GetLastError(), std::system_category());
             return std::nullopt;
         }
 
@@ -501,10 +638,10 @@ struct Async_TCPSocket
         const int error = ::ioctlsocket(handle, FIONBIO, &flags);
         if (error)
         {
+            ec = std::error_code(::WSAGetLastError(), std::system_category());
             return std::nullopt;
         }
 
-        std::error_code ec;
         iocp.associate_socket(wi::WinSOCKET(handle), kClientKeyIOCP, ec);
         if (ec)
         {
@@ -516,6 +653,12 @@ struct Async_TCPSocket
     Sender_Connect async_connect(Endpoint_IPv4 endpoint)
     {
         return Sender_Connect{_socket, endpoint};
+    }
+
+    // https://www.boost.org/doc/libs/1_77_0/doc/html/boost_asio/reference/basic_socket_acceptor/async_accept/overload3.html.
+    Sender_Accept<Async_TCPSocket> async_accept()
+    {
+        return Sender_Accept<Async_TCPSocket>{_socket, *_iocp};
     }
 
     // https://www.boost.org/doc/libs/1_77_0/doc/html/boost_asio/reference/basic_stream_socket/async_write_some.html.
@@ -545,9 +688,9 @@ struct Async_TCPSocket
             return;
         }
         int error = ::shutdown(socket, SD_BOTH);
-        assert(error == 0);
+        (void)error;
         error = ::closesocket(socket);
-        assert(error == 0);
+        (void)error;
         _iocp = nullptr;
     }
 
@@ -746,61 +889,115 @@ int main()
     assert(!ec);
     assert(iocp);
 
-    std::optional<Async_TCPSocket> socket = Async_TCPSocket::make(*iocp);
-    assert(socket);
+    std::optional<Async_TCPSocket> client_socket = Async_TCPSocket::make(*iocp, ec);
+    assert(!ec);
+    assert(client_socket);
+
+    std::optional<Async_TCPSocket> server_socket = Async_TCPSocket::make(*iocp, ec);
+    assert(!ec);
+    assert(server_socket);
 
     auto endpoint = Endpoint_IPv4::from_string("127.0.0.1", 60260);
     assert(endpoint);
 
-#define XX_MAYBE_TRIGGER_STACK_OVERFLOW() 0
-
-#if (!XX_MAYBE_TRIGGER_STACK_OVERFLOW())
-    char write_data[] = "Test";
-    char read_data[sizeof(write_data)]{};
-#else
-    // Send a lot of data to echo server, local host.
-    // That most likely leads to some sockets reads operations
-    // to complete immediately.
     std::vector<char> write_data;
-    write_data.resize(1 * 1024 * 1024 * 1024, 'x');
+    write_data.resize(1ull * 1024, 'x');
     std::vector<char> read_data;
     read_data.resize(write_data.size());
-#endif
 
-#if (XX_MAYBE_TRIGGER_STACK_OVERFLOW())
+    std::vector<char> server_data;
+    server_data.resize(write_data.size());
+
+#if (1)
     unifex::trampoline_scheduler scheduler(128/*max depth*/);
 #else
     // Stackoverflow may be in async_read().
     unifex::inline_scheduler scheduler;
 #endif
 
-    // RUN python.exe tpc_server.py.
-    auto logic = [&]()
+    // https://docs.microsoft.com/en-us/windows/win32/winsock/using-so-reuseaddr-and-so-exclusiveaddruse.
+    int enable_reuse = 1;
+    error = ::setsockopt(server_socket->_socket, SOL_SOCKET, SO_REUSEADDR
+        , reinterpret_cast<char*>(&enable_reuse), sizeof(enable_reuse));
+    assert(error == 0);
+
+    struct sockaddr_in server_address{};
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = endpoint->_ip_network;
+    server_address.sin_port = endpoint->_port_network;
+    error = ::bind(server_socket->_socket
+        , reinterpret_cast<SOCKADDR*>(&server_address)
+        , sizeof(server_address));
+    assert(error == 0);
+
+    error = ::listen(server_socket->_socket, 16);
+    assert(error == 0);
+
+    struct ServerState
     {
-        return unifex::sequence(
-              socket->async_connect(*endpoint)
-                | unifex::then([](void_)
+        Async_TCPSocket _client;
+    };
+
+    auto server_logic = [&]()
+    {
+        return unifex::let_value(unifex::just(ServerState())
+            , [&](ServerState& state)
+        {
+            return unifex::sequence(
+                server_socket->async_accept()
+                    | unifex::then([&state](Async_TCPSocket client)
                 {
-                    printf("Connected!.\n");
+                    state._client = std::move(client);
+                    printf("[Server] Accepted.\n");
                 })
-            , async_write(*socket, write_data)
-                    | unifex::then([](std::size_t bytes_transferred)
-                {
-                    printf("Sent %i bytes!.\n", int(bytes_transferred));
-                })
-            , async_read(*socket, read_data, scheduler)
+                , async_read(state._client, server_data, scheduler)
                     | unifex::then([&](std::size_t bytes_transferred)
                 {
-                    printf("Received %i bytes.\n", int(bytes_transferred));
+                    printf("[Server] Received %zu bytes.\n", bytes_transferred);
                 })
+                , async_write(state._client, server_data)
+                    | unifex::then([&state](std::size_t bytes_transferred)
+                {
+                    printf("[Server] Sent %zu bytes.\n", bytes_transferred);
+                    state._client.disconnect();
+                    (void)state;
+                })
+                );
+        });
+    };
+
+    auto client_logic = [&]()
+    {
+        return unifex::sequence(
+            client_socket->async_connect(*endpoint)
+                | unifex::then([](void_)
+            {
+                printf("[Client] Connected.\n");
+            })
+            , async_write(*client_socket, write_data)
+                | unifex::then([](std::size_t bytes_transferred)
+            {
+                printf("[Client] Sent %zu bytes.\n", bytes_transferred);
+            })
+            , async_read(*client_socket, read_data, scheduler)
+                | unifex::then([&](std::size_t bytes_transferred)
+            {
+                printf("[Client] Received %zu bytes.\n", bytes_transferred);
+            })
             );
     };
 
-    bool finish = false;
-    auto state = unifex::connect(logic(), StopReceiver{finish});
-    unifex::start(state);
+    const auto start = std::chrono::steady_clock::now();
 
-    while (!finish)
+    bool finish_server = false;
+    auto server_state = unifex::connect(server_logic(), StopReceiver{finish_server});
+    unifex::start(server_state);
+
+    bool finish_client = false;
+    auto client_state = unifex::connect(client_logic(), StopReceiver{finish_client});
+    unifex::start(client_state);
+
+    while (!finish_client || !finish_server)
     {
         wi::PortEntry entries[4];
         for (const wi::PortEntry& entry : iocp->get_many(entries, ec))
@@ -812,6 +1009,15 @@ int main()
         }
     }
 
-    socket->disconnect();
+    const auto end = std::chrono::steady_clock::now();
+    const float seconds = std::chrono::duration_cast<std::chrono::duration<float>>(end - start).count();
+    printf("Elapsed: %.3f seconds.\n", seconds);
+
+    assert(memcmp(write_data.data(), read_data.data(), write_data.size()) == 0);
+    assert(memcmp(write_data.data(), server_data.data(), write_data.size()) == 0);
+
+    client_socket->disconnect();
+    server_socket->disconnect();
+
     WSACleanup();
 }
